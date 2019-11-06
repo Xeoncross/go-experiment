ca4b239e35890d957cb890b4d0d88a8c348efb21
xeoncross <david@xeoncross.com>
2019-11-06 17:29:47 -0600

    Testing lookup time for millions of strings

--- internal/millionstrings/README.md
+++ internal/millionstrings/README.md
@@ -9,3 +9,26 @@ map[string]int      ~= 78MB (notice int32 cost)
 map[string]struct{} ~= 61MB (empty struct{})
 []string            ~= 33MB
 ```
+
+Furthermore, how much faster is a binary search over a map lookup?
+
+With 1 million 20 byte strings
+
+```
+BenchmarkBinarySearch-8   	 3361562	       322 ns/op	      24 B/op	       2 allocs/op
+BenchmarkMapSearch-8      	 4069456	       284 ns/op	      24 B/op	       2 allocs/op
+```
+
+And 1 million 40 byte strings
+
+```
+BenchmarkBinarySearch-8   	 2769397	       392 ns/op	      56 B/op	       2 allocs/op
+BenchmarkMapSearch-8      	 3997479	       293 ns/op	      56 B/op	       2 allocs/op
+```
+
+And 1 million hashes
+
+```
+BenchmarkBinarySearch-8   	  922741	      1279 ns/op	     212 B/op	       5 allocs/op
+BenchmarkMapSearch-8      	 2426865	       467 ns/op	     212 B/op	       5 allocs/op
+```
--- internal/millionstrings/millionstrings_test.go
+++ internal/millionstrings/millionstrings_test.go
@@ -1,8 +1,12 @@
 package millionstrings
 
 import (
+	"crypto/sha1"
+	"encoding/base64"
+	"encoding/binary"
 	"fmt"
 	"runtime"
+	"sort"
 	"testing"
 )
 
@@ -12,9 +16,9 @@ func reportMemory(name string) {
 	fmt.Printf("%20s MB: %5.2f\n", name, float64(m.Alloc)/1024/1024)
 }
 
-func TestMillionStrings(t *testing.T) {
+var size = 1000000
 
-	size := 1000000
+func TestMillionStrings(t *testing.T) {
 
 	fmt.Printf("Estimated size of %d strings: MB: %5.2f\n", size, float64(size)*10/1024/1024) // 10 bytes long
 
@@ -47,4 +51,75 @@ func TestMillionStrings(t *testing.T) {
 
 	reportMemory("[]string")
 
+}
+
+func BenchmarkBinarySearch(b *testing.B) {
+
+	var list = make([]string, size)
+	for i := 0; i < size; i++ {
+		// list[i] = fmt.Sprintf("%40d", i)
+		list[i] = hashKey(i)
+	}
+
+	sort.Strings(list)
+
+	b.ResetTimer()
+	var found int
+	for i := 0; i < b.N; i++ {
+		// key := fmt.Sprintf("%40d", i)
+		key := hashKey(i)
+
+		i := sort.SearchStrings(list, key)
+		if i < len(list) && list[i] == key {
+			found++
+		}
+	}
+
+	want := b.N
+	if want > size {
+		want = size
+	}
+
+	if found != want {
+		b.Errorf("Search failure: got %d, want %d", found, want)
+	}
+
+}
+
+func BenchmarkMapSearch(b *testing.B) {
+
+	var hash = make(map[string]struct{}, size)
+	for i := 0; i < size; i++ {
+		// hash[fmt.Sprintf("%40d", i)] = struct{}{}
+		hash[hashKey(i)] = struct{}{}
+	}
+
+	b.ResetTimer()
+	var found int
+	for i := 0; i < b.N; i++ {
+		// key := fmt.Sprintf("%40d", i)
+		key := hashKey(i)
+
+		if _, ok := hash[key]; ok {
+			found++
+		}
+	}
+
+	want := b.N
+	if want > size {
+		want = size
+	}
+
+	if found != want {
+		b.Errorf("Map search failure: got %d, want %d", found, want)
+	}
+
+}
+
+func hashKey(i int) string {
+	var b = make([]byte, 4)
+	binary.BigEndian.PutUint32(b, uint32(i))
+	hasher := sha1.New()
+	hasher.Write(b)
+	return base64.URLEncoding.EncodeToString(hasher.Sum(nil))
 }
